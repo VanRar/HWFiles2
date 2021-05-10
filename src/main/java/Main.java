77dab4fcae11fa993c6722fb68294264cf33ed9e
VanRar <vitalikf7@gmail.com>
2021-05-10 15:01:06 +0300

    СохраниЛ сериализованные объекты GameProgress в папку savegames из предыдущей задачи.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,25 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
     public static void main(String[] args) {
         GameProgress gamer1 = new GameProgress(100, 5, 10, 108);
         GameProgress gamer2 = new GameProgress(99, 3, 2, 10);
         GameProgress gamer3 = new GameProgress(10, 7, 100, 1500);
+        saveGame("E:\\Games\\savegames\\gamer1.dat", gamer1);
+        saveGame("E:\\Games\\savegames\\gamer2.dat", gamer2);
+        saveGame("E:\\Games\\savegames\\gamer3.dat", gamer3);
 
     }
+
+    public static void saveGame(String path, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(path);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
+

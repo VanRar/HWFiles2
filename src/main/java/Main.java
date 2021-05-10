04e5f700379733d53a8ba5ce1ec73c777ad8146a
VanRar <vitalikf7@gmail.com>
2021-05-10 14:41:52 +0300

    Созданы три экземпляра класса GameProgress

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        GameProgress gamer1 = new GameProgress(100, 5, 10, 108);
+        GameProgress gamer2 = new GameProgress(99, 3, 2, 10);
+        GameProgress gamer3 = new GameProgress(10, 7, 100, 1500);
+
+    }
+}
